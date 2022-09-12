5f57618dbdbf823984493d7a6a93459134606df5
rcruz_23 <rcruz89@gmail.com>
2022-09-12 00:27:38 -0600

    Initial commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+// Your First Program
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+    }
+}
